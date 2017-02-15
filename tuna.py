cf6e0ce07a0b0ed717607398d1e4b610d3a99756
Kazu Fukushige <k.fukushige@griffith.edu.au>
2017-02-15 17:04:41 +1000

    test tuna

---
+++ tuna.py
@@ -0,0 +1,17 @@
+#
+#
+#
+#
+#
+tuna = "sarmon"
+if tuna == "bass":
+    print 'THis is fish.'
+
+elif tuna == "sarmon":
+    print 'Happy sarmon'
+
+elif tuna == "fish":
+    print 'Yeah this is Tuna'
+
+else:
+    print 'I dont know what is'

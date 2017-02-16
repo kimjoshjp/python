07304d7560ae2f44a5a28bf59bea96f323d07c1e
Kazu Fukushige <k.fukushige@griffith.edu.au>
2017-02-16 15:32:52 +1000

    fish sarmon

--- tuna.py
+++ tuna.py
@@ -2,17 +2,22 @@
 #
 #
 #
-#Loop. if elif else
+# Loop. if elif else
 
-tuna = "sarmon"
-if tuna == "bass":
-    print 'THis is fish.'
+fish_1 = "sarmon"
+fish_2 = "tuna"
 
-elif tuna == "sarmon":
-    print 'Happy sarmon'
+if fish_1 == "sarmon":
+    if fish_2 == "maguro":
+        print 'They are best.'
+    else:
+    	print 'I dont know what is'
 
-elif tuna == "fish":
-    print 'Yeah this is Tuna'
+# elif fish_2 == "tuna":
+#    print 'Happy Tuna'
+#
+# elif fish_1 == "shrimp":
+#    print 'Yeah this is Shrimp'
 
-else:
-    print 'I dont know what is'
+# else:
+#    print 'I dont know what is'

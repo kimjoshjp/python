825e9c8c152bd72da4199d99848871b092b26fbe
Kazu Fukushige <k.fukushige@griffith.edu.au>
2017-03-01 14:06:46 +1000

    add new file

--- read.py
+++ read.py
@@ -6,14 +6,22 @@
 
 # Create File OBJ and sepecify path with write mode
 
-fob = open('/Users/kazu/python/a.txt', 'w')
+fob = open('/Users/kazu/GitHub/python/python/a.txt', 'w')
 fob.write('Welocme to Python world')
 fob.close()
 
+fob = open('/Users/kazu/GitHub/python/python/a.txt', 'r')
+print fob.read()
+
 # Read a.txt with read mode
-fob = open('/Users/kazu/python/server_name.txt', 'r')
+fob = open('/Users/kazu/GitHub/python/python/server.txt', 'r')
 # print fob.read(3)
+
+# Read one line
 print fob.readline()
+
+# Read all lines
+print fob.readlines()
 # read them into xyz
 #xyz = fob.read()
 #print xyz

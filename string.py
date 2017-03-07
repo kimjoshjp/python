f8642f2fde77dc00a08b8526f4fba55479fbf512
Kazu Fukushige <k.fukushige@griffith.edu.au>
2017-03-07 17:17:21 +1000

    new file

---
+++ string.py
@@ -0,0 +1,38 @@
+# git ip
+#
+#
+#
+# String practice
+
+name = "Joshua"
+print("Hello, PyCharm my name is %s! " % name)
+
+years = 19
+print("I am %d years old " % years)
+
+
+# lower, Upper case
+monty_python = "Monty Python"
+print(monty_python)
+print(monty_python.lower())
+print(monty_python.upper())
+
+# String index
+name = "Kazunori"
+print("u is " + name[3])
+
+p_letter = name[0]
+print(p_letter)
+
+# Negative index
+long_string = "This is a very long string!"
+exclamation = long_string[-1]
+print(exclamation)
+
+# String slice
+name = "Fukushige Kazunori"
+fuku = name[:9]
+print(fuku)
+
+last = name[10:]
+print(last)

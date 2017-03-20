d40487b825dea3792cb66ce302fb80243926ed5b
Kazu Fukushige <k.fukushige@griffith.edu.au>
2017-03-20 22:03:13 +1000

    add loop_2

---
+++ loop_2.py
@@ -0,0 +1,15 @@
+#
+#
+#
+#
+#
+mem = {'Josh':18,
+       'Matt':23,
+       'Alex':13,
+       'Kim':40,
+       'Kazu':47,
+       #'Dick':55,
+}
+
+aaa = mem.get('Dick', 'Unknown')
+print ('Dick is %s years old' % aaa)

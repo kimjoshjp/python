a5aee94c289a19a3ff8f511332df6f9723572caa
Kazu Fukushige <k.fukushige@griffith.edu.au>
2017-03-22 22:16:40 +1000

    Adding beer

--- def.py
+++ def.py
@@ -8,15 +8,15 @@ food = ['apple', 'lemon', 'banana', 'water', 'peach']
 
 # Pick up from apple,lemon and banana only.
 for a in food[:3]:
-    print[a]
-    print(len(a))
+    print([a])      #Display apple,lemon and banana
+    print(len(a))   #length of apple,lemmon, banana
 
-print food[0]  # appple
-print food[4]  # peach
+print (food[0])  # appple
+print (food[4])  # peach
 
 food.append("lime")
-print food
+print (food)
 
 # apple in food list , yes
 if 'apple' in food:
-    print "Yeah"
+    print ("Yeah")

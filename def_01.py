e80f706a92666e406415f0b9e688204628c34b4e
Kazu Fukushige <k.fukushige@griffith.edu.au>
2017-03-28 13:25:47 +1000

    def

---
+++ def_01.py
@@ -0,0 +1,16 @@
+#
+#
+#
+#
+# Def with unpaking param
+
+
+def add(*numbers):
+    total = 0
+    for number in numbers:
+        total = total + number
+        return total
+
+add (1,2,3,4,5,6,7,8,9)
+
+

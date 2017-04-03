f7cfd1eceed995b3082c408d5b86da9921972bb4
Kazu Fukushige <k.fukushige@griffith.edu.au>
2017-04-03 22:02:03 +1000

    constructor

--- class_3.py
+++ class_3.py
@@ -9,14 +9,19 @@ class Myclass:
     var_1 = 100
     var_2 = 200
 
-    def foo(self):
-        print("Hello from function foo")
+    def __init__(self, name, age, home, country):
+        self.name = name
+        self.age = age
+        self.home = home
+        self.country = country
 
-obj_1 = Myclass() 	#Create obj_1 from Myclass
-obj_2 = Myclass()	#Second obj_2
+        print("Hello {0} san {1} years old and home town is {2} from {3}."
+              .format(self.name, self.age, self.home, self.country ))
+
+obj_1 = Myclass("Haru", 34, "Tokyo", "Japan") 	#Create obj_1 from Myclass
+obj_2 = Myclass("Kazu", 47, "Brisbane", "Aus")	#Second obj_2
 
 obj_1.var_1 = 400    #Change value
  
 print (obj_1.var_1)		#Display 400
-obj_1.foo() 
 print (obj_2.var_2)		#Display 200

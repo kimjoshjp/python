6e8c544c34f5a9c1263420dae971f0ce928fd52b
Kazu Fukushige <k.fukushige@griffith.edu.au>
2019-01-10 21:46:36 +1000

    add new file

--- module_1.py
+++ module_1.py
@@ -2,8 +2,13 @@
 #
 #
 #
-# Module and pakages
+# 
 
-import datetime
+from datetime import datetime
 
-print (datetime.datetime.today())
+with open('days.txt', encoding='utf-8') as f:
+    for row in f:
+        str_row = row.rstrip()
+        str_row = datetime.strptime(str_row, '%Y-%m-%d')
+        str_row = datetime.strftime(str_row, '%Y/%m/%d')
+        print(str_row)

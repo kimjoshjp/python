dbbf2e512c44b7e5197b0de9b0f111da442c60fa
Kazu Fukushige <k.fukushige@griffith.edu.au>
2019-05-22 20:32:46 +1000

    jj

---
+++ rstrip.py
@@ -0,0 +1,21 @@
+#
+#
+#
+#
+#
+
+import pandas as pd
+"""
+aLdata = []
+
+with open ('sales.csv') as f:
+    for row in f:
+        row_string = row.rstrip()
+
+        aLdata.append(row_string)
+
+print(aLdata)
+"""
+
+df = pd.read_csv('sales.csv')
+print(df)
